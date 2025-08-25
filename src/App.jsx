7a59beb543d716999f7b33e0a3409cab19219d15
Caleb Muturi <calebmuturi2024@gmail.com>
2025-08-25 09:02:21 +0300

    Add Navbar component and integrate it into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
