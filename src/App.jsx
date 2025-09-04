cfa443c111cbfab4e2767cc96739a1a60732ef23
Caleb Muturi <calebmuturi2024@gmail.com>
2025-09-04 21:27:53 +0300

    feat: Add doctor dashboard, appointments, and profile pages with appointment fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorsList from './pages/Admin/DoctorsList';
 import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -24,11 +27,17 @@ const App = () => {
       <div className="flex items-start">
         <Sidebar />
         <Routes>
+          {/* Admin Route */}
           <Route path="/" element={<></>}></Route>
           <Route path="/admin-dashboard" element={<Dashboard />}></Route>
           <Route path="/all-appointments" element={<AllAppointments />}></Route>
           <Route path="/add-doctor" element={<AddDoctor />}></Route>
           <Route path="/doctor-list" element={<DoctorsList />}></Route>
+
+          {/* Doctor Route */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />}></Route>
+          <Route path="/doctor-appointments" element={<DoctorAppointments />}></Route>
+          <Route path="/doctor-profile" element={<DoctorProfile />}></Route>
         </Routes>
       </div>
     </div>
